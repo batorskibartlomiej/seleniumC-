29b5fc13cda289ddb74940fe9c46d4a8085155a9
Bartek <batorskib@gmail.com>
2025-07-19 13:56:44 +0200

    Action class/drag and drop

--- Selenium Learning/AlertsActionsAutoSuggestive.cs
+++ Selenium Learning/AlertsActionsAutoSuggestive.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +24,7 @@ namespace Selenium_Learning
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
             driver = new ChromeDriver();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
             driver.Manage().Window.Maximize();
 
@@ -39,13 +41,65 @@ namespace Selenium_Learning
             driver.FindElement(By.Id("name")).SendKeys(name);
             driver.FindElement(By.Id("confirmbtn")).Click();
 
-            string alertText = driver.SwitchTo().Alert().Text;
+            string alertText = driver.SwitchTo().Alert().Text;//to pobeira tekst alertu
             driver.SwitchTo().Alert().Accept();
             //driver.SwitchTo().Alert().Dismiss();
            // driver.SwitchTo().Alert().SendKeys("test"); 
 
             StringAssert.Contains(name, alertText);
 
+        }
+
+        [Test]
+        public void test_AutoSuggestiveDropDowns()
+        {
+
+            driver.FindElement(By.Id("autocomplete")).SendKeys("ind");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+                .ElementIsVisible(By.CssSelector(".ui-menu-item div")));
+
+            
+
+            IList<IWebElement> options=  driver.FindElements(By.CssSelector(".ui-menu-item div"));
+
+            //String expectedCountry = "India";
+
+            foreach (IWebElement option in options)
+
+            {
+                if (option.Text.Equals("India"))
+                {
+                    option.Click();
+                }
+                TestContext.WriteLine(driver.FindElement(By.CssSelector(".ui-menu-item div")).Text);
+
+            }
+
+            Assert.That(driver.FindElement(By.Id("autocomplete")).GetAttribute("value"), Is.EqualTo("India"));
+
+              
+        }
+
+        [Test]
+        public void test_Action()
+        {
+            driver.Url = "https://rahulshettyacademy.com/";
+            Actions a = new Actions(driver);
+            a.MoveToElement(driver.FindElement(By.PartialLinkText("More"))).Perform();
+            Thread.Sleep(1000);
+            a.MoveToElement(driver.FindElement(By.XPath("//ul[@class='dropdown-menu']/li[1]/a"))).Click().Perform();
+
+        }
+
+
+        [Test]
+        public void test_action_dragAndDrop()
+        {
+            driver.Url = "https://demoqa.com/droppable/";
+            Actions a = new Actions(driver);
+            a.DragAndDrop(driver.FindElement(By.Id("draggable")), driver.FindElement(By.Id("droppable"))).Perform();
+            Thread.Sleep(1000);
 
         }
 
